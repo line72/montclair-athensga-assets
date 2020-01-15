3cb17393bd16100b273e0c90f6a2447d2b690135
Marcus Dillavou <line72@line72.net>
2020-01-15 17:32:07 -0600

    Initial commit

---
+++ assets/Configuration.js
@@ -0,0 +1,31 @@
+/* -*- Mode: rjsx -*- */
+
+/*******************************************
+ * Copyright (2018)
+ *  Marcus Dillavou <line72@line72.net>
+ *  http://line72.net
+ *
+ * Montclair:
+ *  https://github.com/line72/montclair
+ *  https://montclair.line72.net
+ *
+ * Licensed Under the GPLv3
+ *******************************************/
+
+import AvailtecParser from './AvailtecParser';
+
+class Configuration {
+    constructor() {
+        // Athens, GA
+        this.center = [33.958824, -83.377606];
+
+        this.agencies = [
+            {
+                name: 'Routes',
+                parser: new AvailtecParser('https://athensga.gotransitapp.com/api/no.php/InfoPoint')
+            }
+        ];
+    }
+}
+
+export default Configuration;
